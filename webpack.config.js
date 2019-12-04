ed6f86fd6162420da5352cae04cdcb58f5ecfb90
Danny Wieser <danny.wieser@seequent.com>
2019-12-03 20:26:21 -0700

    Initial commit: linter setup, webpack, basic dependencies

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html',
+      filename: './index.html',
+    }),
+  ],
+};
