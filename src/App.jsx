5de991b7cd28d970567f0d282114b1f84da2c34e
Danny Wieser <danny.wieser@seequent.com>
2019-12-03 21:33:04 -0700

    start to fill out content

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './index.css';
 import { Content } from './components';
 
+// to try: https://codepen.io/ogre14t/pen/KmWjeo
+
 const App = () => (
   <div className="App">
     <div className="header">
       <h1>the investment experiment</h1>
+      <span className="nav">about</span>
+      <span className="nav">results</span>
+      <span className="nav">transactions</span>
+      <span className="nav">blog</span>
     </div>
     <Content />
   </div>
